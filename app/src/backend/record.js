0f9b55178f403433e7f02716c3fb4aa2563d1735
nickandless0 <tomas90@gmail.com>
2022-09-15 11:09:47 -0300

    api key

--- app/src/backend/record.js
+++ app/src/backend/record.js
@@ -1,7 +1,12 @@
 const express = require('express');
 require("dotenv").config();
-const { getNft } = require('./backendHelper')
+const {
+    getNft
+} = require('./backendHelper')
 
+const apiKeys = [
+    '29df7ed6c340e52066fbe1f3d0d3505019863bc6'
+]
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /listings.
@@ -9,6 +14,12 @@ const recordRoutes = express.Router();
 
 // This section will help you get a list of all the documents.
 recordRoutes.route("/mintToken").get(async function (req, res) {
+    if (apiKeys.includes(req.body.apiKey)) {
+        // Authorize access
+    } else {
+        res.status(401).send('unauthorized');
+    }
+
     let result = req.query
     console.log("body", req);
     res.json(result)
